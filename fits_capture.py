8b812632c1c2586114298d7f3b338757f426c10b
Tom Badran <tom@badrunner.net>
2013-07-01 11:02:29 +0100

    Added command line program to pull images from camera

---
+++ fits_capture.py
@@ -0,0 +1,22 @@
+from allsky import AllSkyCamera
+import serial
+import argparse, sys
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', help='Path to serial device', default='/dev/usbserial')
+    parser.add_argument('-e', '--exposure', type=float, help='Exposure time in seconds', default=1.0)
+    parser.add_argument('path', help='Filename to save image')
+    args = parser.parse_args()
+
+    try:
+        cam = AllSkyCamera(args.device)
+        print('Downloading image ...')
+        image = cam.get_image(exposure=args.exposure)
+        image.writeto(args.path)
+    except serial.serialutil.SerialException as err:
+        print(str(err))
+        sys.exit(2)
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = 'pyallsky',
     version = '0.1dev',
+    entry_points = {
+        'console_scripts': [
+            'fits-capture = fits_capture:main'
+        ]
+    },
     packages = find_packages(exclude='web')
 )
